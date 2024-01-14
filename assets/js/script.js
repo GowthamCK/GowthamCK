158982cfab8a2ad5de012e6c511508349869ade4
GOWTHAM C K <104862882+GowthamCK@users.noreply.github.com>
2024-01-15 01:59:47 +0530

    adding bot

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,13 +195,12 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/65a4360a0ff6374032c032de/1hk4n66i6';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
 })();
 // End of Tawk.to Live Chat
-
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
